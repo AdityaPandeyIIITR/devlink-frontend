55e6e4f661b3210a2cb5bcc5cf6b507a14517db1
Aditya Pandey <pandeyaditya12828@gmail.com>
2025-07-14 20:23:52 +0530

    Set up entry point and basic routes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import AnalyticsPage from './pages/AnalyticsPage';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/analytics/:shortId" element={<AnalyticsPage />} />
+    </Routes>
+  );
+};
+
+export default App;
